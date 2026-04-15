2090b55c0cbb6c4b0c278f7258394a744e344035
HarshSen-hub <142962897+HarshSen-hub@users.noreply.github.com>
2026-04-15 18:32:48 +0530

    Update student_routes.py

--- student_routes.py
+++ student_routes.py
@@ -1,4 +1,4 @@
- from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 import pandas as pd
 import os
 
